36e7bd23631b30052b9a2ecaa55bdbe2b98c7d74
chandradip2006 <chandradipkarmakar2006@gmail.com>
2025-07-17 15:47:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http=require("http");
+
+const express=require('express');
+const app=express();
+const path=require('path')
+const {Server}=require('socket.io');
+
+const server=http.createServer(app);
+const io=new Server(server);
+// socket.io
+io.on('connection' , (client)=>{
+    // console.log('Client connected' , client.id);
+    client.on('chatMessage' , (message)=>{
+        // console.log('A new message' , message)
+        io.emit('serverMessage' , message);
+    })
+})
+
+
+const PORT=process.env.PORT;
+app.use(express.static('./public'));
+
+app.get('/' , (req , res)=>{
+    return res.sendFile("./public/index")
+})
+
+server.listen(PORT , ()=>console.log("Server started at PORT:" , PORT));
